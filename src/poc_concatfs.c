433d40ba05b268f14c5786fe932201b69a592114
Peter Schlaile <peter@schlaile.de>
2015-11-22 19:39:08 +0100

    Funktioniert jetzt halbwegs, mnt-dir Berechtigungen sind noch Banane

--- src/poc_concatfs.c
+++ src/poc_concatfs.c
@@ -43,7 +43,7 @@
 #include <sys/types.h>
 #include <dirent.h>
 
-static const char *src_dir = 0;
+static char src_dir[PATH_MAX];
 
 struct chunk {
 	struct chunk * next;
@@ -97,7 +97,7 @@ static void open_files_push_front(struct concat_file * cf)
 	lock();
 
 	cf->next = open_files;
-	open_files = cf->next;
+	open_files = cf;
 
 	unlock();
 }
@@ -111,7 +111,7 @@ static struct concat_file * open_files_erase(int fd)
 
 	if (open_files && open_files->fd == fd) {
 		rv = open_files;
-		open_files = open_files->next;
+		open_files = rv->next;
 	} else {
 		for (p = open_files; p; p = p->next) {
 			if (p->next && p->next->fd == fd) {
@@ -125,7 +125,9 @@ static struct concat_file * open_files_erase(int fd)
 		}
 	}
 
-	rv->next = 0;
+	if (rv) {
+		rv->next = 0;
+	}
 
 	unlock();
 
@@ -143,7 +145,7 @@ static struct concat_file * open_concat_file(int fd, const char * path)
 	
 	FILE * fp;
 
-	if (fd != 0) {
+	if (fd >= 0) {
 		fp = fdopen(dup(fd), "r");
 	} else {
 		fp = fopen(path, "r");
@@ -175,13 +177,13 @@ static struct concat_file * open_concat_file(int fd, const char * path)
 		} else {
 			snprintf(tpath, sizeof(tpath), "%s/%s",base_dir, fpath);
 		}
-		if (lstat(tpath, &stbuf) == 0) {
+		if (stat(tpath, &stbuf) == 0) {
 			rv->fsize += stbuf.st_size;
 		} else {
 			continue;
 		}
 
-		if (fd) {
+		if (fd >= 0) {
 			c_n = (struct chunk *) calloc(sizeof(struct chunk), 1);
 
 			c_n->fsize = stbuf.st_size;
@@ -227,7 +229,7 @@ static void close_concat_file(struct concat_file * cf)
 
 static off_t get_concat_file_size(const char * path)
 {
-	struct concat_file * c = open_concat_file(0, path);
+	struct concat_file * c = open_concat_file(-1, path);
 	off_t rv;
 
 	if (!c) {
@@ -255,7 +257,9 @@ static int read_concat_file(int fd, void *buf, size_t count, off_t offset)
 		return 0;
 	}
 	
-	for (c = cf->chunks; c && offset > c->fsize; c = c->next) {
+	c = cf->chunks;
+
+	for (; c && offset > c->fsize; c = c->next) {
 		offset -= c->fsize;
 	}
 
@@ -293,16 +297,36 @@ static int is_concatfs_file(const char * path)
 	return (strstr(path, "-concat-") != 0);
 }
 
+static int concatfs_readlink(const char *path, char *link, size_t size)
+{
+	int rv = 0;
+	char fpath[PATH_MAX];
 
+	snprintf(fpath, sizeof(fpath), "%s/%s", src_dir, path);
+    
+	rv = readlink(fpath, link, size - 1);
+	if (rv < 0) {
+		rv = -errno;
+	} else {
+		link[rv] = '\0';
+		rv = 0;
+	}
+	
+	return rv;
+}
 static int concatfs_getattr(const char *path, struct stat *stbuf)
 {
+	char fpath[PATH_MAX];
+    
+	snprintf(fpath, sizeof(fpath), "%s/%s", src_dir, path);
+
 	memset(stbuf, 0, sizeof(struct stat));
 
-	if (lstat(path, stbuf) != 0)
+	if (lstat(fpath, stbuf) != 0)
 		return -errno;
 	
 	if (is_concatfs_file(path)) {
-		stbuf->st_size = get_concat_file_size(path);
+		stbuf->st_size = get_concat_file_size(fpath);
 	} 
 
 	return 0;
@@ -357,14 +381,20 @@ static int concatfs_open(const char *path, struct fuse_file_info *fi)
 
 	fi->fh = fd;
 
-	open_files_push_front(open_concat_file(fd, fpath));
+	if (is_concatfs_file(path)) {
+		open_files_push_front(open_concat_file(fd, fpath));
+	}
 
 	return 0;
 }
 
 static int concatfs_release(const char * path, struct fuse_file_info * fi)
 {
-	close_concat_file(open_files_erase(fi->fh));
+	if (is_concatfs_file(path)) {
+		close_concat_file(open_files_erase(fi->fh));
+	} else {
+		close(fi->fh);
+	}
 
 	return 0;
 }
@@ -388,6 +418,7 @@ static int concatfs_read(const char *path, char *buf, size_t size, off_t offset,
 
 static struct fuse_operations concatfs_oper = {
 	.getattr	= concatfs_getattr,
+	.readlink       = concatfs_readlink,
 	.readdir	= concatfs_readdir,
 	.open		= concatfs_open,
 	.read		= concatfs_read,
@@ -405,8 +436,19 @@ int main(int argc, char **argv)
 	if (argc < 3) {
 		usage();
 	}
-	
-	src_dir = strdup(argv[1]);
+
+	if (argv[1][0] == '/') {
+		strncpy(src_dir, argv[1], sizeof(src_dir));
+	} else {
+		char cwd[PATH_MAX];
+
+		getcwd(cwd, sizeof(cwd));
+
+		snprintf(src_dir, sizeof(src_dir), "%s/%s",
+			 cwd, argv[1]);
+	}
+
+	fprintf(stderr, "Using src_dir = %s\n", src_dir);
 
 	pthread_mutex_init(&the_lock, NULL);
 
@@ -414,7 +456,7 @@ int main(int argc, char **argv)
 
 	argv_[0] = argv[0];
 
-	memcpy(argv_ + 1, argv + 1, (argc - 2) * sizeof(char*));
+	memcpy(argv_ + 1, argv + 2, (argc - 2) * sizeof(char*));
 
 	return fuse_main(argc - 1, argv_, &concatfs_oper, NULL);
 }
